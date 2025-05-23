485489159ad2a0d4d94dd582d853c2c5851fed08
email <senac@et.intechinfo.fr>
2025-05-23 02:00:44 +0200

    GetTransaction

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,14 +5,63 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Category;
+use Symfony\Component\Security\Core\Security;
 
 final class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    #[Route('/category', name: 'app_category', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): Response
     {
+        $categories = $em->getRepository(Category::class)->findAll();
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/create', name: 'category_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em, Security $security): Response
+    {
+        $user = $security->getUser();
+        if (!$user || !in_array('ROLE_ADMIN', $user->getRoles())) {
+            return $this->redirectToRoute('app_category');
+        }
+        if ($request->isMethod('POST')) {
+            $name = $request->request->get('name');
+            if ($name) {
+                $category = new Category();
+                $category->setName($name);
+                $em->persist($category);
+                $em->flush();
+                return $this->redirectToRoute('app_category');
+            }
+        }
+        return $this->render('category/create.html.twig');
+    }
+
+    #[Route('/category/edit/{id}', name: 'category_edit', methods: ['GET', 'POST'])]
+    public function edit(int $id, Request $request, EntityManagerInterface $em, Security $security): Response
+    {
+        $user = $security->getUser();
+        if (!$user || !in_array('ROLE_ADMIN', $user->getRoles())) {
+            return $this->redirectToRoute('app_category');
+        }
+        $category = $em->getRepository(Category::class)->find($id);
+        if (!$category) {
+            return $this->redirectToRoute('app_category');
+        }
+        if ($request->isMethod('POST')) {
+            $name = $request->request->get('name');
+            if ($name) {
+                $category->setName($name);
+                $em->flush();
+                return $this->redirectToRoute('app_category');
+            }
+        }
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
         ]);
     }
 }
--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -3,29 +3,57 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use App\Entity\Transaction;
+use App\Repository\UserRepository;
 
 final class TransactionController extends AbstractController
 {
     #[Route('/api/transaction', name: 'create_transaction', methods: ['POST'])]
-public function create(Request $request, EntityManagerInterface $em, Security $security): JsonResponse
-{
-    $data = json_decode($request->getContent(), true);
-    $user = $security->getUser();
+    public function create(Request $request, EntityManagerInterface $em, Security $security): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $user = $security->getUser();
 
-    $transaction = new Transaction();
-    $transaction->setUser($user);
-    $transaction->setAmount($data['amount']);
-    $transaction->setCurrency($data['currency']);
-    $transaction->setDate(new \DateTime($data['date']));
-    $transaction->setDescription($data['description']);
-    
+        $transaction = new Transaction();
+        $transaction->setUserId($user);
+        $transaction->setAmountOriginal($data['amount']);
+        $transaction->setCurrencyOriginal($data['currency']);
+        $transaction->setDate(new \DateTime($data['date']));
+        $transaction->setDescription($data['description']);
+        $transaction->setConvertedAmount(0);
+        $transaction->setConversionRate(1);
+        
 
-    $em->persist($transaction);
-    $em->flush();
+        $em->persist($transaction);
+        $em->flush();
 
-    return new JsonResponse(['status' => 'Transaction created'], 201);
-}
+        return new JsonResponse(['status' => 'Transaction created'], 201);
+    }
 
+    #[Route('/api/transaction', name: 'list_transactions', methods: ['GET'])]
+    public function list(EntityManagerInterface $em, TokenStorageInterface $tokenStorage): JsonResponse
+    {
+        $user = $tokenStorage->getToken()?->getUser();
+        $transactions = $em->getRepository(Transaction::class)->findBy(['userId' => $user]);
+
+        $data = [];
+        foreach ($transactions as $transaction) {
+            $data[] = [
+                'id' => $transaction->getId(),
+                'amount' => $transaction->getAmountOriginal(),
+                'currency' => $transaction->getCurrencyOriginal(),
+                'date' => $transaction->getDate()->format('Y-m-d'),
+                'description' => $transaction->getDescription(),
+            ];
+        }
+
+        return new JsonResponse($data);
+    }
 }
